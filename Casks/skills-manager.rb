21dde23ab329ba9ce4bee795ff5a42dcbef59e02
yozlog <241652649+yozlog@users.noreply.github.com>
2026-08-12 01:45:43 +0000

    chore(skills-manager): bump version to 1.32.0

--- Casks/skills-manager.rb
+++ Casks/skills-manager.rb
@@ -1,9 +1,9 @@
 cask "skills-manager" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.31.0"
-  sha256 arm:   "48e4f3681c6318d99da7b02c10565564f09e8cc9d1113f9a73f3e1a12701984a",
-         intel: "13f8cfd910c91b01dc24883161a370f7e209d171997333d9656001a5d6c92f08"
+  version "1.32.0"
+  sha256 arm:   "570e471b58a151955ddd91eb9c2a87a09f48d967984c97a075d18df981a172bf",
+         intel: "956a7bad08c660912661c12848abe02b5a1c10399a5153fd7b0528c24ad6cddc"
 
   url "https://github.com/xingkongliang/skills-manager/releases/download/v#{version}/skills-manager_#{version}_#{arch}.dmg"
   name "Skills Manager"
