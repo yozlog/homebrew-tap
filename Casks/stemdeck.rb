82ff5c9ac34767d31a06f262134a53d945587aff
yozlog <241652649+yozlog@users.noreply.github.com>
2026-09-01 03:36:01 +0000

    chore(stemdeck): bump version to 0.16.0

--- Casks/stemdeck.rb
+++ Casks/stemdeck.rb
@@ -1,9 +1,9 @@
 cask "stemdeck" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.15.2"
-  sha256 arm:   "e191e8ebd884e83c20f8edc901aae405d934ccca500a0f7b0c5a93d04f18c339",
-         intel: "b7e2208ccb129feea43bf38f06a78bfc16ebaad96e353f9b39c3d7c9e4e5955f"
+  version "0.16.0"
+  sha256 arm:   "90cce3721ccb3687d75e3f632cac0eb70f44e5ff83b23ce0611f67e783994e11",
+         intel: "41a74aae91f0600c8188d00340ece4a72b4ff92ee6285f26f18e051457c61b2a"
 
   url "https://github.com/stemdeckapp/stemdeck/releases/download/v#{version}/StemDeck-macOS-#{arch}.dmg"
   name "StemDeck"
